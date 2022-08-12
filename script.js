e31e3c1b77022a71b483fd6de6cdcee82e21730b
DBalint88 <dbalint88@gmail.com>
2022-08-12 09:52:20 -0400

    Add anchor ID's and Initialize JS variables

---
+++ script.js
@@ -0,0 +1,18 @@
+let playerChoice;
+let computerChoice;
+let result;
+let playerScore = 0;
+let computerScore = 0;
+
+let nextRoundButton = document.getElementById("next-round")
+let newGameButton = document.getElementById("new-game")
+let rockButton = document.getElementById("rock-button")
+let paperButton = document.getElementById("paper-button")
+let scissorsButton = document.getElementById("scissors-button")
+let playChoiceSpan = document.getElementById("player-choice")
+let computerChoiceSpan = document.getElementById("computer-choice")
+let resultSpan = document.getElementById("result")
+let playerScoreSpan = document.getElementById("player-score")
+let computerScoreSpan = document.getElementById("computer-score")
+
+
